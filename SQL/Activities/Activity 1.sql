43b983a116a5982df2ad99b1bd1d53249cbf75b1
AradhyulaSowmya <107185094+AradhyulaSowmya@users.noreply.github.com>
2022-06-30 15:58:59 +0530

    Add files via upload

---
+++ SQL/Activities/Activity 1.sql
@@ -0,0 +1,8 @@
+REM   Script: Activity 1
+REM   Activity 1
+
+CREATE TABLE salesman(salesman_id  int primary key, salesman_name varchar2(20),salesman_city varchar2(20),commission int);
+
+DESCRIBE salesman;
+
+
